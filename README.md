4f1e35b7976631cbeaaf77dc3736754439f3282c
Jesus-fran <jesusfran.dev@gmail.com>
2023-05-29 23:58:49 -0600

    se agrega un readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Este es un pequeño programa que realiza una suma de dos numeros enteros.
+
+### Realizado por Jesus Francisco
--- suma.py
+++ suma.py
@@ -2,5 +2,5 @@ numero1 = input('Por favor ingrese el primer número: ')
 numero2 = input('Por favor ingrese el segundo número: ')
 
 resultado = int(numero1) + int(numero2);
-
+# Realizado por Jesus francisco
 print("El resultado es: "+str(resultado))
