cfcde24856d445fb97228ae9c7d9406ec45c2a1c
Jesus-fran <jesusfran.dev@gmail.com>
2023-05-29 23:57:06 -0600

    se crea el proyecto

---
+++ suma.py
@@ -0,0 +1,6 @@
+numero1 = input('Por favor ingrese el primer número: ')
+numero2 = input('Por favor ingrese el segundo número: ')
+
+resultado = int(numero1) + int(numero2);
+
+print("El resultado es: "+str(resultado))
